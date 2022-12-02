90b601a555fc5f93a813ecb739a0f948da2b607d
Ygor Geurts <ygor.geurts@gmail.com>
2022-12-02 20:13:30 +0100

    improvement of day1

--- day01/Program.fs
+++ day01/Program.fs
@@ -4,8 +4,7 @@ let elves =
     System.IO.File.ReadAllText("input.txt")
     |> String.split "\n\n"
     |> List.map (String.split "\n" >> List.sumBy int)
+    |> List.sortDescending
 
-let top3 = List.sortDescending >> List.take 3
-
-printfn $"Part 1: %i{List.max elves}"
-printfn $"Part 2: %i{List.sum (top3 elves)}"
+printfn $"Part 1: %i{List.head elves}"
+printfn $"Part 2: %i{List.sum (List.take 3 elves)}"
