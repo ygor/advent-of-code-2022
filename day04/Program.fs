5bc877dffd620dbd35da228c7e39bbda768b576e
Ygor Geurts <ygor.geurts@gmail.com>
2022-12-04 10:58:59 +0100

    formatting

--- day04/Program.fs
+++ day04/Program.fs
@@ -10,7 +10,6 @@ let parsePair input =
 let pairs = File.ReadAllLines("input.txt") |> Seq.map parsePair
 
 let contains ((x, y), (a, b)) = x <= a && y >= b || a <= x && b >= y 
-
 let overlaps ((x, y), (a, b)) = (x <= b && y >= a) || (a <= y && b >= x) 
 
 printfn $"Part 1: %i{pairs |> Seq.lengthBy contains}"
