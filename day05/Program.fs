35a806bb16ece24062ed79487ff4cfd68e8d7885
Ygor Geurts <ygor.geurts@gmail.com>
2022-12-05 21:17:44 +0100

    improve readability

--- day05/Program.fs
+++ day05/Program.fs
@@ -1,24 +1,28 @@
 ﻿open System.IO
 open Extensions
 
-let parse retain input =
-    input
-    |> Seq.fold (fun crates line ->
-        match line with
-        | Regex.Matches "(?>(\[.\]|\s\s\s)\s?)" items ->
-            items
-            |> List.indexed
-            |> List.fold (fun crates' (i, crate) ->
-                if String.trim crate = "" then crates'
-                else List.updateAt i (crates'[i] @ [crate]) crates') crates 
-        | Regex.Groups "move (\d*) from (\d*) to (\d*)" [ amount; source; dest ] ->
-             let moved = List.take (int amount) crates[int source - 1] 
-             crates
-             |> List.updateAt (int source - 1) (List.skip (int amount) crates[int source - 1])
-             |> List.updateAt (int dest - 1) ((if retain then moved else List.rev moved) @ crates[int dest - 1])            
-        | _ -> crates) (List.init 9 (fun _ -> []))
+let setup stacks crates =
+    crates
+    |> List.indexed
+    |> List.fold (fun stacks' (i, crate) ->
+        if String.trim crate = "" then stacks'
+        else List.updateAt i (stacks'[i] @ [crate]) stacks') stacks     
 
-let rearrange retain = File.ReadAllLines("input.txt") |> parse retain |> List.map List.head
+let move (stacks: string list list) amount source dest retain =
+     let moved = List.take amount stacks[source]
+     stacks
+     |> List.updateAt source (List.skip amount stacks[source])
+     |> List.updateAt dest ((if retain then moved else List.rev moved) @ stacks[dest])            
+
+let rearrange retain =
+    File.ReadAllLines("input.txt")
+    |> Seq.fold (fun stacks line ->
+        match line with
+        | Regex.Matches "(?>(\[.\]|\s\s\s)\s?)" crates -> setup stacks crates 
+        | Regex.Groups "move (\d*) from (\d*) to (\d*)" [ amount; source; dest ] ->
+            move stacks (int amount) (int source - 1) (int dest - 1) retain
+        | _ -> stacks) (List.init 9 (fun _ -> []))
+    |> List.map List.head
 
 printfn $"Part 1: %A{rearrange false}"
 printfn $"Part 2: %A{rearrange true}"
