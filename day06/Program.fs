2639937862414d0377d12babd405f676588cf4a7
Ygor Geurts <ygor.geurts@gmail.com>
2022-12-06 06:18:18 +0100

    simplify

--- day06/Program.fs
+++ day06/Program.fs
@@ -3,9 +3,7 @@
 let marker length =
     File.ReadAllText("input.txt")
     |> Seq.windowed length
-    |> Seq.indexed
-    |> Seq.find (fun (_, chars) -> Seq.distinct chars |> Seq.length = length)
-    |> fst
+    |> Seq.findIndex (fun chars -> Seq.distinct chars |> Seq.length = length)
   
 printfn $"Part 1: %A{(marker 4) + 4}"
 printfn $"Part 2: %A{(marker 14) + 14}"
