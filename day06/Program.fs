5b542d13d94e52f77cda4ace2d86b205343cf257
Ygor Geurts <ygor.geurts@gmail.com>
2022-12-06 06:15:49 +0100

    day06

---
+++ day06/Program.fs
@@ -0,0 +1,11 @@
+﻿open System.IO
+
+let marker length =
+    File.ReadAllText("input.txt")
+    |> Seq.windowed length
+    |> Seq.indexed
+    |> Seq.find (fun (_, chars) -> Seq.distinct chars |> Seq.length = length)
+    |> fst
+  
+printfn $"Part 1: %A{(marker 4) + 4}"
+printfn $"Part 2: %A{(marker 14) + 14}"
